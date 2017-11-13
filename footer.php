a2c0ba237cc58a92aa1f41926d443da687067ef1
dpammyb <dp.brown@mail.utoronto.ca>
2017-11-12 21:16:49 -0500

    changes

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!-- make an ugly footer -->
 <footer id="footer">
   <h1>FOOTER</h1>
-  <p>Powered by madness</p>
+  <p>Powered by greatness</p>
 </footer>
 <!-- do some Wordpress magic -->
 <?php wp_footer(); ?>
